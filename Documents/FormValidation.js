6ac2109d7d9a19e5470b19fb23702ba77e46e884
Shenobiwan <31104754+Shenobiwan@users.noreply.github.com>
2017-10-23 20:17:20 -0500

    trying to debug

--- Documents/FormValidation.js
+++ Documents/FormValidation.js
@@ -1,13 +1,13 @@
 //Form Validation 
 
-var contactFields = [document.forms["contact-us"] ["name"].value, document.forms["contact-us"] ["email"].value, document.forms["contact-us"] ["body"].value ];
+var contactFields = [document.getElementById("contact-us").elements[0].value, document.getElementById("contact-us").elements[1].value, document.getElementById("contact-us").elements[2].value ];
 var missingValues = "";
 
-function validateForm(contact) {
+function validateForm() {
 
-    for(var i = 0; i < contact.length; i++) {
-        if(contact[i] == " ") {
-            missingValues += contact[i] + "\n";    
+    for(var i = 0; i < contactFields.length; i++) {
+        if(contactFields[i] == " ") {
+            missingValues += contactFields[i] + "\n";    
         }
     alert("Form values are missing, please enter your: \n" + missingValues);
     }
