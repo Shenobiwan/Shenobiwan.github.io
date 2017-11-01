e2732a6da55b820908c6237d518b930e3c19f250
Shenobiwan <31104754+Shenobiwan@users.noreply.github.com>
2017-10-31 20:48:59 -0500

    test alert

--- Documents/FormValidation.js
+++ Documents/FormValidation.js
@@ -18,6 +18,7 @@ function validateForm() {
     nameValidation();
 }
 function emailValidation() {
+    alert("TEST Your email is not formatted correctly. Please correct and resubmit the form.");
     var patt = new RegExp("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$");
     var emailField = document.forms["contact-us"] ["email address"].value;
     if(patt.test(emailField) == true && emailField !== "") {
