5013dc1ddf796b5d823383cbc2a697ef943208c9
Shenobiwan <31104754+Shenobiwan@users.noreply.github.com>
2017-10-31 20:45:11 -0500

    test in IFelse

--- Documents/FormValidation.js
+++ Documents/FormValidation.js
@@ -11,6 +11,8 @@ function validateForm() {
     }
     if(missingValues == null || missingValues == "") {
     } else { alert("Form values are missing, please enter your: \n" + missingValues);
+            emailValidation();
+            nameValidation();
     }
     emailValidation();
     nameValidation();
