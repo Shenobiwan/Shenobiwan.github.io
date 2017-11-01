43db9696aa35af5de4e53c26269c58671a1e4874
Shenobiwan <31104754+Shenobiwan@users.noreply.github.com>
2017-10-31 21:15:53 -0500

    double slash

--- Documents/FormValidation.js
+++ Documents/FormValidation.js
@@ -16,10 +16,10 @@ function validateForm() {
     nameValidation();
 }
 function emailValidation() {
-    alert("TEST Your email is not formatted correctly. Please correct and resubmit the form.");
-    var patt = new RegExp("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$");
+    
+    var patt = new RegExp("[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,3}$");
     var emailField = document.forms["contact-us"] ["email address"].value;
-    if(patt.test(emailField) == true && emailField !== "") {
+    if !(patt.test(emailField)) {
             alert("Your email is not formatted correctly. Please correct and resubmit the form.");
         }
     }
