311a1b22d41c4d5099b7ad4d061b26586a746850
Shenobiwan <31104754+Shenobiwan@users.noreply.github.com>
2017-10-31 21:46:39 -0500

    test

--- Documents/FormValidation.js
+++ Documents/FormValidation.js
@@ -25,7 +25,7 @@ function emailValidation() {
     }
 function nameValidation() {
     
-    var patt = new RegExp ("/[A-Za-z]+[\s]+[A-Za-z?]/");
+    var patt = new RegExp ("/[A-Za-z\sA-Za-z?]/");
     var nameField = document.forms["contact-us"] ["name"].value;
     if(patt.test(nameField) == false) {
            alert("Your name cannot contain numbers or symbols. Please correct and resubmit the form.");
