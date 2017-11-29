d5979c467e2f5e29e5a89c606892c0243d46f00b
Shenobiwan <31104754+Shenobiwan@users.noreply.github.com>
2017-11-28 21:08:49 -0600

    test

--- Space Race/game.js
+++ Space Race/game.js
@@ -1,3 +1,10 @@
-function loadScreen() {
-  alert("This is working");
+function goScotty() {
+  document.getElementbyId("picard").style.visibility = "hidden";
+  document.getElementbyId("introText").style.visibility = "hidden";
+  document.getElementbyId("go").style.visibility = "hidden";
+ 
+ document.body.style.backgroundImage = url("http://avante.biz/wp-content/uploads/Deep-Space-Wallpapers/Deep-Space-Wallpapers-037.jpg");
+  document.getElementbyId("borg").style.visibility = "visible";
+  document.getElementbyId("enterprise").style.visibility = "visible";
+    alert("This is being called");
 }
