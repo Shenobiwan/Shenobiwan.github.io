5a35041acfeede9b314e7ff35ede3ddc31e56308
Shenobiwan <31104754+Shenobiwan@users.noreply.github.com>
2017-12-05 11:32:14 -0600

    test

--- Space Race/game.js
+++ Space Race/game.js
@@ -6,13 +6,9 @@ function goScotty() {
   document.getElementById("introText").style.visibility = "hidden";
   document.getElementById("go").style.visibility = "hidden";
   document.getElementById("headline").style.visibility = "hidden";
-
- 
-  document.getElementById("borg").style.visibility = "visible";
-  document.getElementById("enterprise").style.visibility = "visible";
   var list = document.getElementsByTagName("div")[2];
   list.style.backgroundColor = " ";
-  list.style.backgroundImage = "url('https://avante.biz/wp-content/uploads/Deep-Space-Wallpapers/Deep-Space-Wallpapers-037.jpg')";
+  list.style.backgroundImage = "url("http://thewallpaper.co/wp-content/uploads/2016/10/Free-space-backgrounds-pictures-desktop-download-high-definiton-wallpapers-desktop-images-windows-10-backgrounds-colourful-4k-download-wallpapers-hi-res-quality-images-1920x1080.jpg")";
   document.getElementById("red").style.visibility = "visible";
   document.getElementById("redalertmsg").style.visibility = "visible";
   document.getElementById("redmsg").style.visibility = "visible";
