d1e4e7a7b076f8bf168615a5c474509ec10df561
Shenobiwan <31104754+Shenobiwan@users.noreply.github.com>
2017-12-05 11:52:23 -0600

    test

--- Space Race/game.js
+++ Space Race/game.js
@@ -1,5 +1,7 @@
 var intro = document.getElementById("go");
+var redButton = document.getElementById("redalertmsg");
 intro.onclick = goScotty;
+redButton.onlick = 
 
 function goScotty() {
   document.getElementById("picard").style.visibility = "hidden";
@@ -8,8 +10,15 @@ function goScotty() {
   document.getElementById("headline").style.visibility = "hidden";
   var list = document.getElementsByTagName("div")[2];
   list.style.backgroundColor = " ";
-  list.style.backgroundImage = "url("http://thewallpaper.co/wp-content/uploads/2016/10/Free-space-backgrounds-pictures-desktop-download-high-definiton-wallpapers-desktop-images-windows-10-backgrounds-colourful-4k-download-wallpapers-hi-res-quality-images-1920x1080.jpg")";
+  list.style.backgroundImage = "url('http://thewallpaper.co/wp-content/uploads/2016/10/Free-space-backgrounds-pictures-desktop-download-high-definiton-wallpapers-desktop-images-windows-10-backgrounds-colourful-4k-download-wallpapers-hi-res-quality-images-1920x1080.jpg')";
   document.getElementById("red").style.visibility = "visible";
   document.getElementById("redalertmsg").style.visibility = "visible";
   document.getElementById("redmsg").style.visibility = "visible";
 }
+function goGreen() {
+   document.getElementById("red").style.visibility = "hidden";
+  document.getElementById("redmsg").style.visibility = "hidden";
+    document.getElementById("redalertmsg").style.visibility = "hidden";
+   document.getElementById("green").style.visibility = "visible";
+  document.getElementbyId("greenmsg").style.visibility = "visible";
+  document.getElementbyId("greenalertmsg").style.visibility = "visible";
